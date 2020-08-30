d1e1105ee3f8dea3a8cacc650916ece0782b30e2
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-08-30 01:44:10 +0000

    Automatically backed up by Learn

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,21 +1,11 @@
 
-export function fetchCats() {
-
-  return function(dispatch){
-    dispatch({type: 'LOADING_CATS'})
-    return fetch('http://localhost:4000/db')
-      .then(res => {
-        return res.json()
-      }).then(responseJson => {
-        dispatch({type: 'FETCH_CATS', payload: responseJson.images})
+export const fetchCats = () => {
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_CATS'})
+    fetch('https://learn-co-curriculum.github.io/cat-api/cats.json').then(response => {
+      return response.json()
+    }).then(responseJSON => {
+      dispatch({ type: 'ADD_CATS', cats: responseJSON.images })
     })
-
   }
 }
-
-export function loadingCats(){
-  return {
-    type: 'LOADING_CATS',
-    payload : []
-  };
-};
