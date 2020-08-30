7c5800b65671a09e7878ad13ae1f337ee3ba4a2e
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-08-30 01:38:07 +0000

    Automatically backed up by Learn

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,15 @@
-// write your CatList component here
+import React from 'react';
+
+const CatList = ({ catPics }) => {
+  const renderCats = catPics.map(catPic =>
+    <img key={catPic.id} src={catPic.url} />
+  );
+
+  return (
+    <div>
+      {renderCats}
+    </div>
+  );
+};
+
+export default CatList;
