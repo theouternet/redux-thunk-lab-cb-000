a5dd27d11b0df0d6e2e993ca289622e82384d283
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-08-30 01:36:56 +0000

    Automatically backed up by Learn

--- src/reducers/catsReducer.js
+++ src/reducers/catsReducer.js
@@ -1,1 +1,10 @@
-// add the catsReducer
+	export default (state = {loading: false, pictures: []}, action) => {
+  switch (action.type) {
+    case 'FETCH_CATS':
+      return {loading: false, pictures: action.payload};
+    case 'LOADING_CATS':
+      return {loading: true, pictures: action.payload};
+    default:
+      return state;
+  }
+};
