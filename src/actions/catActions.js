25a483ea6f984032ff34c1bdc7cbd590a2d6d6cd
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-08-30 01:37:56 +0000

    Automatically backed up by Learn

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,1 +1,21 @@
-// write and export your action creator function here
+
+export function fetchCats() {
+
+  return function(dispatch){
+    dispatch({type: 'LOADING_CATS'})
+    return fetch('http://localhost:4000/db')
+      .then(res => {
+        return res.json()
+      }).then(responseJson => {
+        dispatch({type: 'FETCH_CATS', payload: responseJson.images})
+    })
+
+  }
+}
+
+export function loadingCats(){
+  return {
+    type: 'LOADING_CATS',
+    payload : []
+  };
+};
